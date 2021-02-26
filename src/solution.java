32d30cb52bab94850ca264740fa485600c5bda87
ZZOTK <786399012@qq.com>
2021-02-26 12:03:46 +0800

    commit

--- src/solution.java
+++ src/solution.java
@@ -1,79 +1,55 @@
 
-
-import javax.naming.InsufficientResourcesException;
 import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class solution {
-    public int reversePairs(int[] nums) {
-        int len = nums.length;
-
-        if (len < 2) {
-            return 0;
-        }
-
-        int[] copy = new int[len];
-        for (int i = 0; i < len; i++) {
-            copy[i] = nums[i];
-        }
-
-        int[] temp = new int[len];
-        return reversePairs(copy, 0, len - 1, temp);
-    }
-
-    private int reversePairs(int[] nums, int left, int right, int[] temp) {
-        if (left == right) {
-            return 0;
-        }
-
-        int mid = left + (right - left) / 2;
-        int leftPairs = reversePairs(nums, left, mid, temp);
-        int rightPairs = reversePairs(nums, mid + 1, right, temp);
-
-        if (nums[mid] <= nums[mid + 1]) {
-            return leftPairs + rightPairs;
-        }
-
-        int crossPairs = mergeAndCount(nums, left, mid, right, temp);
-        return leftPairs + rightPairs + crossPairs;
-    }
-
-    private int mergeAndCount(int[] nums, int left, int mid, int right, int[] temp) {
-        for (int i = left; i <= right; i++) {
-            temp[i] = nums[i];
-        }
-
-        int i = left;
-        int j = mid + 1;
-
-        int count = 0;
-        for (int k = left; k <= right; k++) {
-
-            if (i == mid + 1) {
-                nums[k] = temp[j];
-                j++;
-            } else if (j == right + 1) {
-                nums[k] = temp[i];
-                i++;
-            } else if (temp[i] <= temp[j]) {
-                nums[k] = temp[i];
-                i++;
-            } else {
-                nums[k] = temp[j];
-                j++;
-                count += (mid - i + 1);
-            }
-        }
-        return count;
-    }
-
     public static void main(String[] args) {
-
+        List<String> list =  new CopyOnWriteArrayList<>();
+        Data data = new Data();
+        new Thread(() -> {
+            for(int i  = 0; i < 10; i ++){
+                try {
+                    data.increment();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"A").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    data.decrement();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"B").start();
+        new Thread(() -> {
+            for(int i  = 0; i < 10; i ++){
+                try {
+                    data.increment();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"C").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    data.decrement();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"D").start();
     }
-
 
     public class TreeNode {
         int val;
@@ -85,13 +61,53 @@ public class solution {
         }
     }
 
-    class ListNode {
+    public class ListNode {
         int val;
         ListNode next;
 
         ListNode(int x) {
             val = x;
             next = null;
+        }
+    }
+}
+
+class Data{
+    private int number = 0;
+
+    Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+
+    public  void increment() throws InterruptedException {
+        try {
+            lock.lock();
+            while(number != 0){
+                condition.await();
+            }
+            number ++;
+            System.out.println(Thread.currentThread().getName() + "->" + number);
+            condition.signalAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+    public synchronized void decrement() throws InterruptedException {
+        try {
+            lock.lock();
+            while (number == 0) {
+                condition.await();
+            }
+            number--;
+            System.out.println(Thread.currentThread().getName() + "->" + number);
+            condition.signalAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
     }
 }
